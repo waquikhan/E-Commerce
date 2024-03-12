5c1ce389fd4f6122adbbc19e2530237a211a43b9
waquikhan <waquiraj313@gmail.com>
2024-03-12 13:04:39 +0530

    product page is add inside the application

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../Context/ShopContext';
+import { useParams } from 'react-router-dom';
+import Breadcrum from '../Components/Breadcrum/Breadcrum';
+import Productdisplay from '../Components/Productdisplay/Productdisplay';
+import DescriptionBox from '../Components/DescriptionBox/DescriptionBox';
+import RelatedProducts from '../Components/RelatedProduct/RelatedProducts';
 
 const Product = () => {
+
+  const {all_product} = useContext(ShopContext);
+  const {productId} = useParams();
+  const product = all_product.find((e)=> e.id === Number(productId))
+
   return (
     <div>
-      
+      <Breadcrum product={product} />
+      <Productdisplay product={product} />
+      <DescriptionBox />
+      <RelatedProducts/>
     </div>
   )
 }
